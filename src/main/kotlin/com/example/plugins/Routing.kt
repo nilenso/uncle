dc59466a6695671db460cb65dbcc7d0cbfb4f767
Tarun <tarun@nilenso.com>
2024-08-28 15:17:35 +0530

    Add GET /advice

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,10 +1,20 @@
 package com.example.plugins
 
+import com.example.repository.AdviceRepository
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+        get("/health") {
+            call.respond(HttpStatusCode.OK, "OK")
+        }
+
+        get("/advice") {
+            val advice = AdviceRepository.getAdvice()
+            call.respondText(advice)
+        }
     }
 }
