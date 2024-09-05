0e9577832d638a78dddb95577b4e2f0414f9f486
Tarun <tarun@nilenso.com>
2024-09-05 15:13:55 +0530

    Add postgres testcontainer
    Add suite level test container dependency

---
+++ webserver/src/test/kotlin/com/nilenso/uncle/webserver/repositories/PostgresAdviceRepositoryTest.kt
@@ -0,0 +1,27 @@
+package com.nilenso.uncle.webserver.repositories
+
+import com.nilenso.uncle.webserver.dto.AdviceDTO
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.exposed.sql.Database
+import org.junit.jupiter.api.BeforeAll
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PostgresAdviceRepositoryTest {
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun setupDb(): Unit {
+            println("start test")
+        }
+    }
+
+    @Test
+    fun addAdviceShouldAddAdvice() {
+        val repository = PostgresAdviceRepository(Database.connect(RepositoryTestSuite.dataSource()))
+        runBlocking {
+            repository.addAdvice(AdviceDTO("This is just a test beta"))
+            assertEquals("This is just a test beta", repository.getAdvice()?.advice, "Inserted advice not returned")
+        }
+    }
+}
